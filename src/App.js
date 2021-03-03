e0e13f36692f1f17c393d967b1595b4d8cdb0524
Your Name <you@example.com>
2021-03-03 15:12:20 +0600

    added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@
 //  import Switch from "./components/Switch1";
  import YourName from "./components/YourName";
  import Massive from "./components/Massive";
+ import Decrise from "./components/Decrise";
+ import Length from "./components/Length";
+ import Colors from "./components/Colors";
+ import InputTypeRange from "./components/InputTypeRange";
+ import InputRate from "./components/InputRate";
+ import Temps from "./components/Temps";
 
 function App() {
 return(
@@ -10,6 +16,12 @@ return(
       <Counter/> */}
       <YourName/>
       <Massive/>
+      <Decrise/>
+      <Length/>
+      <Colors/>
+      <InputTypeRange min={0} max={10} value={3}/>
+      <InputRate/>
+      <Temps/>
 
      </div>
   )
