fa420d6a058225f2eb66e4e7e9527be3e59c63c8
Your Name <you@example.com>
2021-03-11 15:25:53 +0600

    Persons

---
+++ src/10.03.21/Person.js
@@ -0,0 +1,11 @@
+import classes from "./Person.module.css";
+
+const Person = ({ name, age, children, }) => {
+    return (<div className={classes.Person}>
+        <h2 className={classes.name}>{ name }</h2>
+        <h4 className={classes.age}>{ age } years old</h4>
+        <h5 className={classes.hobby}> { children }</h5>
+      <span className="highlight">Hello</span>
+    </div>);
+}
+export default Person;
