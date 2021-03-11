e4d688e6d4c1cfd15cd2ff7267572547c3da3351
Your Name <you@example.com>
2021-03-11 15:25:29 +0600

    Button with de

---
+++ src/10.03.21/Button.js
@@ -0,0 +1,11 @@
+import classes from "./Button.module.css";
+
+const Button = ({ button }) => {
+    return (
+     
+     <button className={classes.Button}>
+         Sumbit</button>
+ 
+    );
+}
+export default Button;
