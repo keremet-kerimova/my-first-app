96def640f45eb7eea6b058740b5136d81aea7810
Your Name <you@example.com>
2021-03-12 15:30:53 +0600

    added Appp js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
 
-// import Person from "./10.03.21/Person";
-// import Button from "./10.03.21/Button";
-// import SimpleButton from "./10.03.21/SimpleButton";
-// import "./App.css";
-// import PinkButton from "./10.03.21/PinkButton";
-// import RedButton from "./10.03.21/RedButton";
-// import GreenButton from "./10.03.21/GreenButton/GreenButton";
+import Person from "./13.03.21/Person";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Person name="Elon Baike" age={47}>
-//         Ice skating, Eating donuts, Dancing under the moon
-//       </Person>
-//       <Person name="Bill Baike" age={50}>
-//         Saving the world
-//       </Person>
-//       <span className="highlight">Hello</span>
-//       <Button className="Button">Sumbit</Button>
-//       <SimpleButton>Style#1</SimpleButton>
-//       <PinkButton>Style#2</PinkButton>
-//       <RedButton>Style#3</RedButton>
-//       <GreenButton>Style#4</GreenButton>
-
-
-
-//     </div>
-//   );
-// }
-// export default App;
-
-import Person from "./10.03.21/Person";
-import "./App.css";
-import PersonList from "./10.03.21/PersonList/PersonList";
 function App() {
   const persons = [
-    { name: "Elon Baike", age: 47, hobby: "Ice skating, Eating donuts, Dancing under the moon" },
-    { name: "Bill Baike", age: 50, hobby: "Saving the world" }
+    { name: "Elaman", role: "Teacher", wpm: 10, commits: 100 },
+    { name: "Choro", role: "Assistant", wpm: 78, commits: 10401 },
+    { name: "Tynchtyk", role: "Teacher", wpm: 102, commits: 0 },
+    { name: "Danila", role: "Student", wpm: -5, commits: 1244 },
+    { name: "Madina", role: "Student", wpm: 30, commits: 5000 },
+    { name: "Tatyna", role: "Student", wpm: 27, commits: 572541 },
+    { name: "Emirlan", role: "Student", wpm: 20, commits: 56566 },
+    { name: "Baitemir", role: "Student", wpm: 35, commits: 556 },
+    { name: "Yntymak", role: "Student", wpm: 40, commits: 55 },
+    { name: "Tamara", role: "Student", wpm: 32, commits: 999 },
+    { name: "Nurkyz", role: "Student", wpm: 39, commits: 26 },
+    { name: "Kunduz", role: "Student", wpm: 38, commits: 99 },
+    { name: "Adis", role: "Student", wpm: 39, commits: 56 },
+    { name: "Islam", role: "Student", wpm: 20, commits: 25 },
+    { name: "Felix", role: "Student", wpm: 25, commits: 99 },
+    { name: "Erjan", role: "Student", wpm: 19, commits: 9959 },
+    { name: "Ermek", role: "Student", wpm: 28, commits: 6655 },
+    { name: "Aiymkyz", role: "Student", wpm: 37, commits: 226 },
+    { name: "Keremet", role: "Student", wpm: 41, commits: 999 },
+    { name: "Karlygach", role: "Student", wpm: 25, commits: 66 },
+    { name: "Keremet", role: "Student", wpm: 32, commits: 52 },
+    { name: "Kutman", role: "Student", wpm: 33, commits: 99 },
+    { name: "Chyngyz", role: "Student", wpm: 36, commits: 559 },
   ];
+  const output = persons.map(person => <Person {...person} />);
   return (
     <div className="App">
-      <PersonList persons={persons} />
+      {output}
     </div>
   );
 }
