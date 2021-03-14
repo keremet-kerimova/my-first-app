6590c6a7c613f16c912d769d95507be1817c15c0
Your Name <you@example.com>
2021-03-14 21:10:05 +0600

    peson list

--- src/13.03.21/Person.js
+++ src/13.03.21/Person.js
@@ -1,12 +1,13 @@
 import classes from "./Person.module.css";
 
-const Person = ({ name, role, wpm, commits }) => {
+const Person = ({ name, role, photo, wpm, commits }) => {
     return (
         <div className={classes.Person}>
             <div className={classes.name}>{name}</div>
             <div className={classes.role}>{role}</div>
             <div className={classes.wpm}>{wpm}WPM</div>
             <div className={classes.commits}>{commits} commits</div>
+            <div className={classes.divPhoto}><img src={photo} alt="Photo" className={classes.photo}></img></div>
         </div>
     );
 }
