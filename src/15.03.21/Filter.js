8810dd68a5f08f616f293645c2c56e3ce0a546b3
Your Name <you@example.com>
2021-03-15 15:42:39 +0600

    filters

---
+++ src/15.03.21/Filter.js
@@ -0,0 +1,9 @@
+import classes from "./Filter.module.css";
+
+const Filter = ({ state, change, children}) => {
+    return ( 
+        <div classes={classes.Filter}></div>
+     );
+}
+ 
+export default Filter;
